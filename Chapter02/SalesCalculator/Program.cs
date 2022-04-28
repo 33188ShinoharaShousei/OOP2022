419da5d58888b6952054268596a9f786d69d2b81
33188 篠原　翔聖 <33188@ojs.ac.jp>
2022-04-28 15:59:11 +0900

    varに変更（暗黙の型指定）

--- Chapter02/SalesCalculator/Program.cs
+++ Chapter02/SalesCalculator/Program.cs
@@ -9,9 +9,9 @@ namespace SalesCalculator {
     class Program {
         static void Main(string[] args) {
             //ReadSales("sales.csv");
-            SalesCounter sales = new SalesCounter("sales.csv");
-            IDictionary<string, int> amountPerStore = sales.GetPerStoreSales();
-            foreach (KeyValuePair<string,int> obj in amountPerStore)
+            var sales = new SalesCounter("sales.csv");
+            var amountPerStore = sales.GetPerStoreSales();
+            foreach (var obj in amountPerStore)
             {
                 Console.WriteLine("{0}　{1}",obj.Key,obj.Value);
             }
