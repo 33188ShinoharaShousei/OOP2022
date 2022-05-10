5f2476c2726ece798b301c1879f29c6337dabc43
33188 篠原　翔聖 <33188@ojs.ac.jp>
2022-05-10 15:59:39 +0900

    問題2.1.4完成

--- Chapter02/Exercise/Program.cs
+++ Chapter02/Exercise/Program.cs
@@ -7,6 +7,38 @@ using System.Threading.Tasks;
 namespace Exercise {
     class Program {
         static void Main(string[] args) {
+            //List<Song> songs = new List<Song>();
+
+            //var song1 = new Song("Let it be","The Beatles",243);
+            //songs.Add(song1);
+
+            //var song2 = new Song("Let it be", "The Beatles", 243);
+            //songs.Add(song2);
+
+            //var song3 = new Song("Let it be", "The Beatles", 243);
+            //songs.Add(song3);
+
+            var songs = new Song[]
+            {
+                new Song("Let it be","The Beatles",256),
+                new Song("マカロニえんぴつ","生きるをする",243),
+                new Song("マカロニえんぴつ","なんでもないよ",258),
+                new Song("マカロニえんぴつ","恋人ごっこ",354),
+                new Song("マカロニえんぴつ","遠心",654),
+
+            };
+
+            PrintSongs(songs);
+        }
+
+        private static void PrintSongs(Song[] songs) {
+            foreach (var song in songs)
+            {
+                Console.WriteLine("{0},{1},{2:mm\\:ss}",
+                    song.Title, song.ArtistName, TimeSpan.FromSeconds(song.Length));
+            }
+
+           
         }
     }
 }
