9c89327f4de6da38d53552e1c7ab9e26a21e29d1
33188 篠原　翔聖 <33188@ojs.ac.jp>
2022-05-17 15:38:16 +0900

    P.100   3-2-1完成

--- Chapter03/Exercise01/Program.cs
+++ Chapter03/Exercise01/Program.cs
@@ -9,13 +9,35 @@ namespace Exercise01 {
         static void Main(string[] args) {
             var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
 
-            Exercise1_1(numbers);
+           // Exercise1_1(numbers);
             Console.WriteLine("");
 
-            Exercise1_2(numbers);
+           // Exercise1_2(numbers);
             Console.WriteLine("");
 
+            Exercise1_3(numbers);
+            Console.WriteLine("");
 
+            Exercise1_4(numbers);
+            
+
+
+        }
+
+        private static void Exercise1_3(List<int> numbers) {
+            foreach (var n in numbers.Where(n => n >= 50))
+            {
+                Console.WriteLine(n);
+            }
+        }
+
+        private static void Exercise1_4(List<int> numbers) {
+            var list = numbers.Select(n => n * 2);
+            numbers[5] = 5000;
+            foreach(var n in list)
+            {
+                Console.WriteLine(n);
+            }
         }
 
         private static void Exercise1_1(List<int> numbers) {
