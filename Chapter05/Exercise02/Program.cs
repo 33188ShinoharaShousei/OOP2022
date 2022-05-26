749572894e2eec29ab200487c8c9b344e07f008d
33188 篠原　翔聖 <33188@ojs.ac.jp>
2022-05-26 15:52:25 +0900

    第５章　演習問題4-1、4-2完成

---
+++ Chapter05/Exercise02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            int number;
+            var inputString = Console.ReadLine();
+            
+            int.TryParse(inputString, out number);
+            Console.WriteLine(number.ToString("#,0"));
+            
+
+
+        }
+    }
+}
