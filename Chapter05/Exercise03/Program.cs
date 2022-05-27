9adc31d0824d8ed6e1ad8abacd15e3738f8b2fb1
33188 篠原　翔聖 <33188@ojs.ac.jp>
2022-05-27 15:58:28 +0900

    演習問題5-4途中

--- Chapter05/Exercise03/Program.cs
+++ Chapter05/Exercise03/Program.cs
@@ -28,25 +28,35 @@ namespace Exercise03 {
 
         private static void Exercise3_1(string text) {
             var count = text.Count(t => t == ' ');
-            Console.WriteLine(count);
+            Console.WriteLine("空白数：{0}", count);
         }
 
         private static void Exercise3_2(string text) {
             var before = text;
-            var after = before.Replace("big","small");
+            var after = before.Replace("big", "small");
             Console.WriteLine(after);
         }
 
         private static void Exercise3_3(string text) {
-            
+            var words = text.Split(' ').Count();
+            Console.WriteLine("単語数：{0}", words);
         }
 
         private static void Exercise3_4(string text) {
-            
+            var words = text.Split(' ').Where( w => w.Length <= 4);
+            foreach (var w in words) {
+                Console.WriteLine(w);
+            }
         }
 
         private static void Exercise3_5(string text) {
-            
+            var words = text.Split(' ');
+            var sb = new StringBuilder();
+            foreach (var w in words) {
+                sb.Append(w);
+            }
+            var u = sb.ToString();
+            Console.WriteLine(u);
         }
     }
 }
