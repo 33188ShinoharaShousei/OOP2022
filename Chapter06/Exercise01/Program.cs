2fc725e3bc50c467cb3927adb2a087e38070a3f2
33188 篠原　翔聖 <33188@ojs.ac.jp>
2022-05-31 15:48:44 +0900

    6章6-1完成

---
+++ Chapter06/Exercise01/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+            var numbers = new int[] { 5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35 };
+            #region
+            Exercise1_1(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_2(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_3(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_4(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_5(numbers);
+            #endregion
+        }
+
+        private static void Exercise1_1(int[] numbers) {
+            //最大値
+            Console.WriteLine(numbers.Max());
+        }
+
+        private static void Exercise1_2(int[] numbers) {
+            //最後から2つの要素取り出し
+
+            for (int i = numbers.Length - 2; i < numbers.Length; i++) {
+                Console.WriteLine(numbers[i]);
+            }
+
+        }
+
+        private static void Exercise1_3(int[] numbers) {
+            //数値を文字列へ変換
+        }
+
+        private static void Exercise1_4(int[] numbers) {
+            //小さい順に並べて先頭の3つを出力
+        }
+
+        private static void Exercise1_5(int[] numbers) {
+            //重複を排除し10より大きい数がいくつあるか
+        }
+    }
+}
