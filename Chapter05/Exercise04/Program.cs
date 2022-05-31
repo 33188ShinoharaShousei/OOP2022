96a453e14baa0d4508dd13c09797c92fcf77d4cc
33188 篠原　翔聖 <33188@ojs.ac.jp>
2022-05-31 13:59:17 +0900

    5章5-4終了

--- Chapter05/Exercise04/Program.cs
+++ Chapter05/Exercise04/Program.cs
@@ -8,18 +8,20 @@ namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
-            
-
+            foreach(var obj in line.Split(';')) {
+                var array = obj.Split('=');
+                Console.WriteLine("{0}:{1}",ToJapanese(array[0]),array[1]);
+            }
         }
 
         static string ToJapanese(string key) {
             switch (key) {
                 case "Novelist":
-                    return "作家 : 谷崎潤一郎";
+                    return "作家 ";
                 case "BestWork":
-                    return "代表作 : 春琴抄";
+                    return "代表作";
                 case "Born":
-                    return "誕生年 : 1886年";
+                    return "誕生日";
             }
             throw new ArgumentException("引数keyは、正しい値ではありません");
         }
