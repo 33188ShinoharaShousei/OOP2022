bc8be0b03a3b7a94bfd2ae88149437449dc153ac
33188 篠原　翔聖 <33188@ojs.ac.jp>
2022-07-04 11:57:39 +0900

    演習問題11-1

---
+++ Chapter11/Exercise1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Linq;
+
+namespace Exercise1 {
+    class Program {
+        static void Main(string[] args) {
+            var file = "sample.xml";
+            Exercise1_1(file);
+            Console.WriteLine();
+            Exercise1_2(file);
+            Console.WriteLine();
+            Exercise1_3(file);
+            Console.WriteLine();
+
+            var newfile = "sports.xml";
+            Exercise1_4(file, newfile);
+        }
+
+        private static void Exercise1_1(string file) {
+
+            var xdoc = XDocument.Load("Sample.xml");
+            var Sample = xdoc.Root.Elements()
+                                .Select(x => new {
+                                    Name = (string)x.Element("name"),
+                                    Teammembers = (String)x.Element("teammembers"),
+                                });
+            foreach (var sample in Sample) {
+                Console.WriteLine("{0} {1}", sample.Name, sample.Teammembers);
+            }
+        }
+
+        private static void Exercise1_2(string file) {
+
+            var xdoc = XDocument.Load("Sample.xml");
+            var Sample = xdoc.Root.Elements()
+                                .OrderByDescending(x => (String)(x.Element("name").Attribute("firstplayed")));
+            foreach (var sample in Sample) {
+                var xfirstplayed = sample.Element("firstplayed");
+                var xname = sample.Element("name");
+                Console.WriteLine("{0}({1})", xname.Value,xfirstplayed.Value);
+            }
+        }
+
+        private static void Exercise1_3(string file) {
+
+        }
+
+        private static void Exercise1_4(string file, string newfile) {
+        }
+    }
+}
--- Chapter11/XmlSample/Program.cs
+++ Chapter11/XmlSample/Program.cs
@@ -10,17 +10,18 @@ namespace XmlSample {
     class Program {
         static void Main(string[] args) {
 
-            using (var wc = new WebClient()) {
-
-                var stream = new WebClient().OpenRead("");
-
-                var xdoc = XDocument.Load(stream);
-                var xNews = xdoc.Root.Descendants("item").Select(x=>x.Element("title"));
-
-                foreach (var date in xNews) {
-                    Console.WriteLine(date);
-                }
+            var xdoc = XDocument.Load("novelists.xml");
+            var novelists = xdoc.Root.Elements()
+                                .Select(x => new {
+                                    Name = (string)x.Element("name"),
+                                    Birth = (DateTime)x.Element("birth"),
+                                    Death = (DateTime)x.Element("death")
+                                });
+            foreach (var novelist in novelists) {
+                Console.WriteLine("{0} ({1}-{2})",
+                                   novelist.Name, novelist.Birth.Year, novelist.Death.Year);
             }
         }
     }
 }
+
