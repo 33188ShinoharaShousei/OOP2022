674a9090b5dafaa8e1e0c5c61283f6ee6aaf5213
33188 篠原　翔聖 <33188@ojs.ac.jp>
2022-10-24 13:10:51 +0900

    WPF　

---
+++ WPF/NumberGame/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NumberGame {
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        Random random = new Random();
+
+        private int correctNumber; //正解ナンバー
+
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            Button bt = (Button)sender;
+            if ((string)bt.Content == correctNumber.ToString()) 
+            //if(int.Parse((string)bt.Content) == correctNumber)
+            {
+                infoDisp.Text = "正解！！！";
+                bt.Background = Brushes.White;
+            }
+            else 
+            {
+                infoDisp.Text 
+                    = int.Parse((string)bt.Content) < correctNumber 
+                                ? "もっと大きい数です" : "もっと小さい数です";
+                bt.Background 
+                    = int.Parse((string)bt.Content) < correctNumber 
+                                ? Brushes.Purple : Brushes.Black;
+            }
+            
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            infoDisp.Text = "ゲームスタート";
+            correctNumber = random.Next(25) + 1; //正解ナンバーを設定
+            this.Title = correctNumber.ToString(); //デバック用で正解を表示
+        }
+    }
+}
