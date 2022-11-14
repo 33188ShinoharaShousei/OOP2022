84baced61691e8d285b1d17001270cde527f561a
33188 篠原　翔聖 <33188@ojs.ac.jp>
2022-11-14 15:59:46 +0900

    途中

---
+++ Chapter14/WeatherApp/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace WeatherApp {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.btWeathorGet = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btWeathorGet
+            // 
+            this.btWeathorGet.Location = new System.Drawing.Point(12, 12);
+            this.btWeathorGet.Name = "btWeathorGet";
+            this.btWeathorGet.Size = new System.Drawing.Size(112, 39);
+            this.btWeathorGet.TabIndex = 1;
+            this.btWeathorGet.Text = "取得";
+            this.btWeathorGet.UseVisualStyleBackColor = true;
+            this.btWeathorGet.Click += new System.EventHandler(this.btWeathorGet_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "地域";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 12);
+            this.label2.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(71, 78);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(149, 20);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btWeathorGet);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btWeathorGet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Chapter14/WeatherApp/Form1.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WeatherApp {
+    public partial class Form1 : Form {
+        //List<string> jsonlist = new List<string>();
+        public Form1() {
+            InitializeComponent();
+              
+        }
+
+        private void btWeathorGet_Click(object sender, EventArgs e) {
+            var wc = new WebClient() {
+                Encoding = Encoding.UTF8
+            };
+            var dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json");
+
+            var json = JsonConvert.DeserializeObject<Class1[]>(dString);
+
+            comboBox1.Items.Add(json[0].timeSeries[0].areas[0].area.name);
+
+            //jsonlist.Add(json.name);
+
+            /*foreach (var b in jsonlist) {
+                comboBox1.Items.Add(b);
+            }*/
+            
+
+        }
+        
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) {
+            
+        }
+    }
+}
