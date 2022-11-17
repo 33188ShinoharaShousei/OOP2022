6aab1d5177904b5be20e07126a448021ed96b73f
33188 篠原　翔聖 <33188@ojs.ac.jp>
2022-11-17 12:00:00 +0900

    コンボボックス　途中

--- Chapter14/WeatherApp/Form1.Designer.cs
+++ Chapter14/WeatherApp/Form1.Designer.cs
@@ -28,6 +28,17 @@ namespace WeatherApp {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btWeathorGet
@@ -43,7 +54,7 @@ namespace WeatherApp {
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(36, 81);
+            this.label1.Location = new System.Drawing.Point(194, 25);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(29, 12);
             this.label1.TabIndex = 2;
@@ -60,23 +71,128 @@ namespace WeatherApp {
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(71, 78);
+            this.comboBox1.Location = new System.Drawing.Point(244, 22);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(149, 20);
             this.comboBox1.TabIndex = 6;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "発表者";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "報告日時";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 12);
+            this.label5.TabIndex = 3;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(10, 195);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 12);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "ヘッドライン";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 252);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(29, 12);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "概要";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(81, 252);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(362, 186);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 155);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 12);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "対象地域";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(81, 65);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(291, 32);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(81, 184);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(291, 32);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(81, 104);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(291, 32);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(81, 144);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(291, 32);
+            this.textBox5.TabIndex = 8;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btWeathorGet);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -87,6 +203,17 @@ namespace WeatherApp {
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
     }
 }
 
--- Chapter14/WeatherApp/Form1.cs
+++ Chapter14/WeatherApp/Form1.cs
@@ -9,7 +9,7 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using WetherApp;
 
 namespace WeatherApp {
     public partial class Form1 : Form {
@@ -23,23 +23,80 @@ namespace WeatherApp {
             var wc = new WebClient() {
                 Encoding = Encoding.UTF8
             };
-            var dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json");
+            var dString = wc.DownloadString("https://www.jma.go.jp/bosai/common/const/area.json");
 
-            var json = JsonConvert.DeserializeObject<Class1[]>(dString);
+            var json = JsonConvert.DeserializeObject<Rootobject>(dString);
 
-            comboBox1.Items.Add(json[0].timeSeries[0].areas[0].area.name);
+            //switch (json) {
+            //    case _010100:
+                    
+            //        break;
 
-            //jsonlist.Add(json.name);
 
-            /*foreach (var b in jsonlist) {
-                comboBox1.Items.Add(b);
-            }*/
-            
+            //}
+
 
         }
         
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) {
-            
+        private void Form1_Load(object sender, EventArgs e) {
+            comboBox1.Items.Add("宗谷地方");
+            comboBox1.Items.Add("上川・留萌地方");
+            comboBox1.Items.Add("網走・北見・紋別地方");
+            comboBox1.Items.Add("十勝地方");
+            comboBox1.Items.Add("釧路・根室地方");
+            comboBox1.Items.Add("胆振・日高地方");
+            comboBox1.Items.Add("石狩・空知・後志地方");
+            comboBox1.Items.Add("渡島・檜山地方");
+            comboBox1.Items.Add("青森県");
+            comboBox1.Items.Add("岩手県");
+            comboBox1.Items.Add("宮城県");
+            comboBox1.Items.Add("秋田県");
+            comboBox1.Items.Add("山形県");
+            comboBox1.Items.Add("福島県");
+            comboBox1.Items.Add("茨城県");
+            comboBox1.Items.Add("栃木県");
+            comboBox1.Items.Add("群馬県");
+            comboBox1.Items.Add("埼玉県");
+            comboBox1.Items.Add("千葉県");
+            comboBox1.Items.Add("東京都");
+            comboBox1.Items.Add("神奈川県");
+            comboBox1.Items.Add("山梨県");
+            comboBox1.Items.Add("長野県");
+            comboBox1.Items.Add("岐阜県");
+            comboBox1.Items.Add("静岡県");
+            comboBox1.Items.Add("愛知県");
+            comboBox1.Items.Add("三重県");
+            comboBox1.Items.Add("新潟県");
+            comboBox1.Items.Add("富山県");
+            comboBox1.Items.Add("石川県");
+            comboBox1.Items.Add("福井県");
+            comboBox1.Items.Add("滋賀県");
+            comboBox1.Items.Add("京都府");
+            comboBox1.Items.Add("大阪府");
+            comboBox1.Items.Add("兵庫県");
+            comboBox1.Items.Add("奈良県");
+            comboBox1.Items.Add("和歌山県");
+            comboBox1.Items.Add("鳥取県");
+            comboBox1.Items.Add("香川県");
+            comboBox1.Items.Add("愛媛県");
+            comboBox1.Items.Add("高知県");
+            comboBox1.Items.Add("山口県");
+            comboBox1.Items.Add("福岡県");
+            comboBox1.Items.Add("佐賀県");
+            comboBox1.Items.Add("長崎県");
+            comboBox1.Items.Add("熊本県");
+            comboBox1.Items.Add("大分県");
+            comboBox1.Items.Add("宮崎県");
+            comboBox1.Items.Add("奄美地方");
+            comboBox1.Items.Add("鹿児島県（奄美地方除く");
+            comboBox1.Items.Add("沖縄本島地方");
+            comboBox1.Items.Add("大東島地方");
+            comboBox1.Items.Add("宮古島地方");
+            comboBox1.Items.Add("八重山地方");
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e) {
+            //textBox2.Text = 
         }
     }
 }
